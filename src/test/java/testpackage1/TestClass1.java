4febcf72a48ba2af11cee46a888fc77b57ecf8b7
A-HEMANTH-SAI <hemanth7automation@gmail.com>
2023-11-29 13:29:13 +0530

    Deleted testNG.xml

--- src/test/java/testpackage1/TestClass1.java
+++ src/test/java/testpackage1/TestClass1.java
@@ -1,9 +1,11 @@
 package testpackage1;
 import org.testng.Reporter;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
-
 import genericutilities.BaseClass;
+import genericutilities.ListenersImpl;
 
+@Listeners(ListenersImpl.class)
 public class TestClass1 extends BaseClass{
 	
 	@Test 
