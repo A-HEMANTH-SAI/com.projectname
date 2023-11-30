86b3f599497904f8e06c1c226938c4cde66d9625
A-HEMANTH-SAI <hemanth7automation@gmail.com>
2023-11-30 14:42:53 +0530

    happy

--- src/main/java/genericutilities/BaseClass.java
+++ src/main/java/genericutilities/BaseClass.java
@@ -13,20 +13,19 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.Parameters;
 public class BaseClass {
 	public WebDriver driver;
 	public static WebDriver ssdriver;
 	public FileInputStream fis;
 	public Properties p;
 	
-	@Parameters("browser")
+
 	@BeforeClass(alwaysRun=true)
-	public void openBrowser(String browser) throws IOException {
+	public void openBrowser() throws IOException {
 		fis=new FileInputStream("./src/test/resources/commondata.property");
 		p=new Properties();
 		p.load(fis);
-		//String browser=p.getProperty("browser");
+		String browser=p.getProperty("browser");
 		if(browser.equalsIgnoreCase("chrome"))
 			driver=new ChromeDriver();
 		else if(browser.equalsIgnoreCase("edge"))
