c774ba60be8736f2a5057441c5da3985e7bf0f74
A-HEMANTH-SAI <hemanth7automation@gmail.com>
2023-11-30 13:10:15 +0530

    asdf

--- src/test/java/testpackage2/TestClass5.java
+++ src/test/java/testpackage2/TestClass5.java
@@ -2,6 +2,8 @@ package testpackage2;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
@@ -9,12 +11,17 @@ public class TestClass5 {
 	
 	@Test
 	public void test1() {
-		WebDriver driver=new ChromeDriver();
-		driver.manage().window().maximize();
+		WebDriver driver;
 		String browser=System.getProperty("BROWSER");
-		driver.get(browser);
+		if(browser.equalsIgnoreCase("chrome"))
+			driver=new ChromeDriver();
+		else if(browser.equalsIgnoreCase("edge"))
+			driver=new EdgeDriver();
+		else driver=new FirefoxDriver();
+		driver.manage().window().maximize();
 		Reporter.log(browser+" OPENED SUCCESSFULLY", true);
-		Reporter.log(System.getProperty("NAME"),true);
+		driver.get("URL");
+		Reporter.log(driver.getTitle(),true);
 		driver.quit();
 		Reporter.log(browser+" CLOSED SUCCESSFULLY",true);
 		
